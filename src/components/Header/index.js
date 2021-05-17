5135b14151bb03ae21cf1359a35fb95a0d172dab
Israel Molestina <molestinaisrael@gmail.com>
2021-05-17 14:51:53 -0400

    started work on home page, added header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div>
+      <header className="row header-background">
+        <h1 className="col-sm-9 text-center display-3">Israel Molestina</h1>
+      </header>
+      <hr />
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div className="container border">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
